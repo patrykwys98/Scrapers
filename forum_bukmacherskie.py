98b0ab0bdeb7c1d7dfadfb74c0888e98354ee754
patrykwys98 <patrykwys98@gmail.com>
2022-05-27 14:00:47 +0200

    Fixing Time Error

--- forum_bukmacherskie.py
+++ forum_bukmacherskie.py
@@ -1,8 +1,44 @@
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import date
+from requests_html import HTMLSession
 
 
 read_file = pd.read_excel('typerzy.xlsx')
 read_file.to_csv('typerzy.csv', index=None, header=True)
 
 df = pd.read_csv('typerzy.csv', delimiter=',')
-print(df)
+
+today = date.today().day
+tomorrow = today + 1
+
+baseurl = "https://forum.bukmacherskie.com/forums/typy-dnia.43"
+r = requests.get(baseurl)
+soup = BeautifulSoup(r.content, 'lxml')
+
+all_links = soup.find_all('a', href=True)
+pages_to_scrap = []
+
+for link in all_links:
+    href = link.get('href')
+    if f"{today}" in href or f"{tomorrow}" in href:
+        pages_to_scrap.append(href.replace("latest", ""))
+
+
+pages_to_scrap = list(dict.fromkeys(pages_to_scrap))
+
+print(pages_to_scrap)
+
+baseurl = "https://forum.bukmacherskie.com"
+for page in pages_to_scrap:
+    s = HTMLSession()
+    r = s.get(baseurl + page)
+    
+
+    r.html.render(timeout=70)
+    soup = BeautifulSoup(r.html.raw_html, "html.parser")
+
+    with open('file.txt', 'w') as file:
+        file.write(soup.prettify())
--- zt.py
+++ zt.py
@@ -44,50 +44,94 @@ for page in pages:
     s = HTMLSession()
     r = s.get(page)
 
-    r.html.render(wait=4)
+    r.html.render(wait=8)
     soup = BeautifulSoup(r.html.raw_html, "html.parser")
 
     bets = soup.find_all(
         'div', id=re.compile("^typ-"), class_="relative")
 
+    print(bets)
+
     for bet in bets:
+
+        fields = bet.find_all("fieldset")
+
         try:
-            fields = bet.find_all("fieldset")
-            formatted_bet = {
-                'effective': bet.find(
-                    'div', class_="absolute top-0 right-[30px] bg-body-lighter shadow shadow-prime-darker rounded-b-md p-1 lg:right-[58px]").find('p', class_="text-[12px] px-1 text-center font-bold lg:text-[14px]").text,
-                'dyscipline':  fields[0].div.text,
-                'match': fields[1].div.text,
-                'prediction': fields[2].div.text,
-                'odds': fields[3].div.text,
-                'start': fields[4].div.text,
-                'bukmacher': fields[5].div.text,
-                'content': bet.find('div', id=re.compile("^content")).text,
-                'author': bet.find('a', class_="block w-[calc(100%_-_75px)] max-w-fit text-ellipsis whitespace-nowrap overflow-hidden !no-underline leading-[1.2] !text-text hover:!text-text-darker").span.text
+            effective = bet.find(
+                'div', class_="absolute top-0 right-[30px] bg-body-lighter shadow shadow-prime-darker rounded-b-md p-1 lg:right-[58px] overflow-hidden after:only-mobile after:absolute after:bg-white after:w-[10%] after:h-full after:top-0 after:right-[150px] after:animate-[like-shine_24s_ease-in-out_infinite] after:blur after:skew-x-12 after:transition-transform").find('p', class_="text-[12px] px-1 text-center font-bold lg:text-[14px]").text
+        except:
+            effective = ""
 
-            }
-            if str(today) in str(page):
-                bet_start_time = timedelta(hours=int(formatted_bet['start'][:2]), minutes=int(
-                    formatted_bet['start'][3:]), seconds=0)
+        try:
+            dyscipline = fields[0].div.text,
+        except:
+            dyscipline = ""
 
-                if (bet_start_time - now).total_seconds() > 0:
-                    if int(formatted_bet.get('effective')[0]) > 6:
-                        value_bets_list.append(formatted_bet)
-                        print("Value bet added")
-                    else:
-                        less_value_bets_list.append(formatted_bet)
-                        print("Less value bet added")
-                else:
-                    recent_bets_list.append(formatted_bet)
-            else:
+        try:
+            match = fields[1].div.text
+        except:
+            match = ""
+
+        try:
+            prediction = fields[2].div.text
+        except:
+            prediction = ""
+
+        try:
+            odds = fields[3].div.text
+        except:
+            odds = ""
+
+        try:
+            start = fields[4].div.text
+        except:
+            start = ""
+
+        try:
+            bukmacher = fields[5].div.text
+        except:
+            bukmacher = ""
+
+        try:
+            content = bet.find('div', id=re.compile("^content")).text
+        except:
+            content = ""
+
+        try:
+            author = bet.find(
+                'a', class_="block w-[calc(100%_-_75px)] max-w-fit text-ellipsis whitespace-nowrap overflow-hidden !no-underline leading-[1.2] !text-text hover:!text-text-darker").span.text
+        except:
+            author = ""
+
+        formatted_bet = {
+            'effective': effective,
+            'dyscipline':  dyscipline,
+            'match': match,
+            'prediction': prediction,
+            'odds': odds,
+            'start': start,
+            'bukmacher': bukmacher,
+            'content': content,
+            'author': author,
+        }
+        start_time = formatted_bet['start'].split(':')
+        if str(today) in str(page):
+            bet_start_time = timedelta(hours=int(start_time[0]), minutes=int(
+                start_time[1]), seconds=0)
+
+            if (bet_start_time - now).total_seconds() > 0:
                 if int(formatted_bet.get('effective')[0]) > 6:
                     value_bets_list.append(formatted_bet)
-                    print("Value bet added")
                 else:
                     less_value_bets_list.append(formatted_bet)
-                    print("Less value bet added")
-        except:
-            print("Something went wrong")
+            else:
+                recent_bets_list.append(formatted_bet)
+        else:
+            if int(formatted_bet.get('effective')[0]) > 6:
+                value_bets_list.append(formatted_bet)
+            else:
+                less_value_bets_list.append(formatted_bet)
+
 
 if not os.path.exists('zt'):
     os.mkdir('zt')
