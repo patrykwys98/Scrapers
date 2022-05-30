e3e03c560cddfd9cc60ec33cfa913f623234e4ca
patrykwys98 <patrykwys98@gmail.com>
2022-05-30 19:24:47 +0200

    Removing multiprocessing

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from multiprocessing import Process
-import sys
+
 from blogabet import scrap_blogabet
 from forum_bukmacherskie import scrap_forum_bukmacherskie
 from zt import scrap_zawod_typer
 
 if __name__ == '__main__':
-    p1 = Process(target=scrap_blogabet())
-    # p2 = Process(target=scrap_forum_bukmacherskie())
-    # p3 = Process(target=scrap_zawod_typer())
-    p1.start()
-    # p2.start()
-    # p3.start()
+    scrap_blogabet()
+    scrap_forum_bukmacherskie()
+    scrap_zawod_typer()
